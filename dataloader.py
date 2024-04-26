a1285eb714a3a8f903e94d733aff4204e9d92a8b
EdZ543 <53500062+EdZ543@users.noreply.github.com>
2024-04-26 13:39:46 -0400

    Ensure test data is not augmented

--- dataloader.py
+++ dataloader.py
@@ -4,7 +4,7 @@ import torch
 from torch.utils.data import random_split, DataLoader
 from torchvision import datasets, transforms
 
-transform = transforms.Compose(
+train_transform = transforms.Compose(
     [
         transforms.ToTensor(),
         # Paper uses per-pixel mean subtraction
@@ -18,13 +18,11 @@ transform = transforms.Compose(
 def get_dataloader(train, batch_size):
     """Get train and test data loaders for CIFAR-10 dataset."""
 
-    # Get data
-    data = datasets.CIFAR10(
-        root="data", train=train, download=True, transform=transform
-    )
-
-    # Create data loaders
     if train:
+        data = datasets.CIFAR10(
+            root="data", train=True, download=True, transform=train_transform
+        )
+
         # Split data into train and validation sets
         generator = torch.Generator().manual_seed(42)
         train_data, val_data = random_split(data, [45000, 5000], generator=generator)
@@ -34,5 +32,11 @@ def get_dataloader(train, batch_size):
 
         return train_dataloader, val_dataloader
     else:
+        # If testing, no transformations
+        data = datasets.CIFAR10(
+            root="data", train=False, download=True, transform=transforms.ToTensor()
+        )
+
         dataloader = DataLoader(data, batch_size=batch_size)
+
         return dataloader
