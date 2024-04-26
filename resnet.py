8109139d945c782642a9a5bca6bf422a9f4e5550
EdZ543 <53500062+EdZ543@users.noreply.github.com>
2024-04-26 11:52:54 -0400

    Implement validation

--- resnet.py
+++ resnet.py
@@ -10,22 +10,16 @@ class ResidualBlock(nn.Module):
     as described in Figure 2 of the paper
     """
 
-    def __init__(self, num_filters, filter_size, subsample=False):
+    def __init__(self, num_filters, subsample=False):
         super().__init__()
         self.subsample = subsample
 
         if subsample:
-            self.conv1 = nn.Conv2d(
-                num_filters // 2, num_filters, filter_size, 2, filter_size // 2
-            )
+            self.conv1 = nn.Conv2d(num_filters // 2, num_filters, 3, 2, 1)
         else:
-            self.conv1 = nn.Conv2d(
-                num_filters, num_filters, filter_size, 1, filter_size // 2
-            )
+            self.conv1 = nn.Conv2d(num_filters, num_filters, 3, 1, 1)
 
-        self.conv2 = nn.Conv2d(
-            num_filters, num_filters, filter_size, 1, filter_size // 2
-        )
+        self.conv2 = nn.Conv2d(num_filters, num_filters, 3, 1, 1)
 
         # The paper uses option A for the shortcut connection, I use option B
         self.projection_shortcut = nn.Conv2d(num_filters // 2, num_filters, 1, 2, 0)
@@ -33,20 +27,19 @@ class ResidualBlock(nn.Module):
     def forward(self, x):
         """Feed forward step"""
 
-        original_x = x
-        x = self.conv1(x)
-        x = F.relu(x)
-        x = self.conv2(x)
+        out = self.conv1(x)
+        out = F.relu(out)
+        out = self.conv2(out)
 
         # Apply projection shortcut if input and output dimensions don't match
         if self.subsample:
-            original_x = self.projection_shortcut(original_x)
+            x = self.projection_shortcut(x)
 
         # Residual connection
-        x += original_x
+        out += x
 
-        x = F.relu(x)
-        return x
+        out = F.relu(out)
+        return out
 
 
 class ResNet(nn.Module):
@@ -58,18 +51,18 @@ class ResNet(nn.Module):
         self.conv1 = nn.Conv2d(3, 16, 3, 1, 1)
 
         # Stack of residual blocks with map size 32 and 16 filters
-        self.stack1 = nn.Sequential(*[ResidualBlock(16, 3) for _ in range(n)])
+        self.stack1 = nn.Sequential(*[ResidualBlock(16) for _ in range(n)])
 
         # Stack of residual blocks with map size 16 and 32 filters
         self.stack2 = nn.Sequential(
-            ResidualBlock(32, 3, subsample=True),
-            *[ResidualBlock(32, 3) for _ in range(n - 1)]
+            ResidualBlock(32, subsample=True),
+            *[ResidualBlock(32) for _ in range(n - 1)]
         )
 
         # Stack of residual blocks with map size 64 and 8 filters
         self.stack3 = nn.Sequential(
-            ResidualBlock(64, 3, subsample=True),
-            *[ResidualBlock(64, 3) for _ in range(n - 1)]
+            ResidualBlock(64, subsample=True),
+            *[ResidualBlock(64) for _ in range(n - 1)]
         )
 
         self.global_avg_pool = nn.AvgPool2d(8)
@@ -78,14 +71,12 @@ class ResNet(nn.Module):
     def forward(self, x):
         """Feed forward step"""
 
-        x = self.conv1(x)
-        x = F.relu(x)
-        x = self.stack1(x)
-        x = self.stack2(x)
-        x = self.stack3(x)
-        x = self.global_avg_pool(x)  # Size is now (batch_size, 64, 1, 1)
-        x = x.view(-1, 64)  # Size is now (batch_size, 64)
-        x = self.fully_connected(
-            x
-        )  # For each sample, takes in 64 inputs (average of each feature map)
-        return x
+        out = self.conv1(x)
+        out = F.relu(out)
+        out = self.stack1(out)
+        out = self.stack2(out)
+        out = self.stack3(out)
+        out = self.global_avg_pool(out)
+        out = out.view(-1, 64)
+        out = self.fully_connected(out)
+        return out
