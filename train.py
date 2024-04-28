ca1c5e880635f4c179b1778ca6ece44c3e56e642
EdZ543 <53500062+EdZ543@users.noreply.github.com>
2024-04-28 11:40:01 -0400

    Simplify file structure

--- train.py
+++ train.py
@@ -1,16 +1,156 @@
 import torch
+from torch import nn
+from torch.optim.lr_scheduler import MultiStepLR
 import wandb
-from train_utils import model_pipeline
 
-wandb.login()
+from dataloader import get_dataloader
+from resnet import ResNet
 
-config = {
-    "n": 3,
-    "batch_size": 128,
-    "learning_rate": 0.1,
-    "epochs": 182,
-    "weight_decay": 0.0001,
-    "momentum": 0.9,
-}
+# Ensure deterministic behavior
+torch.backends.cudnn.deterministic = True
+torch.manual_seed(42)
+torch.cuda.manual_seed(42)
 
-model_pipeline("resnet", config)
+# Device configuration
+device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+
+
+def make(config):
+    train_dataloader, val_dataloader = get_dataloader(True, config.batch_size)
+    test_dataloader = get_dataloader(False, config.batch_size)
+
+    model = ResNet(config.n).to(device)
+
+    loss_func = nn.CrossEntropyLoss()
+    optimizer = torch.optim.SGD(
+        model.parameters(),
+        lr=config.learning_rate,
+        weight_decay=config.weight_decay,
+        momentum=config.momentum,
+    )
+    scheduler = MultiStepLR(optimizer, milestones=[91, 137], gamma=0.1)
+
+    return (
+        model,
+        train_dataloader,
+        val_dataloader,
+        test_dataloader,
+        loss_func,
+        optimizer,
+        scheduler,
+    )
+
+
+def evaluate(model, loader, loss_func):
+    model.eval()
+    loss_sum = 0.0
+    with torch.inference_mode():
+        correct = 0
+        for images, labels in loader:
+            images, labels = images.to(device), labels.to(device)
+
+            # Forward pass
+            outputs = model(images)
+            loss_sum += loss_func(outputs, labels) * labels.size(0)
+
+            _, predicted_indices = torch.max(outputs.data, 1)
+            correct += (predicted_indices == labels).sum().item()
+
+    loss = loss_sum / len(loader.dataset)
+    accuracy = correct / len(loader.dataset)
+
+    return loss, accuracy
+
+
+def train(model, train_loader, val_loader, loss_func, optimizer, scheduler, config):
+    # Tell wandb to watch what the model gets up to: gradients, weights, and more!
+    wandb.watch(model, loss_func, log="all", log_freq=10)
+
+    for epoch in range(config.epochs):
+        model.train()
+
+        # Train for one epoch
+        for images, labels in train_loader:
+            images, labels = images.to(device), labels.to(device)
+
+            outputs = model(images)
+            train_loss = loss_func(outputs, labels)
+            optimizer.zero_grad()
+            train_loss.backward()
+            optimizer.step()
+
+        # Log training and validation metrics
+        train_loss, train_accuracy = evaluate(model, train_loader, loss_func)
+        val_loss, val_accuracy = evaluate(model, val_loader, loss_func)
+        wandb.log(
+            {
+                "epoch": epoch,
+                "train/error": 1 - train_accuracy,
+                "train/loss": train_loss,
+                "validation/error": 1 - val_accuracy,
+                "validation/loss": val_loss,
+            }
+        )
+
+        # Adjust learning rate
+        scheduler.step()
+
+
+def model_pipeline(project, config):
+
+    # tell wandb to get started
+    with wandb.init(project=project, config=dict(config)) as run:
+        # access all HPs through wandb.config, so logging matches execution!
+        config = wandb.config
+
+        # make the model, data, optimizer, and scheduler
+        (
+            model,
+            train_loader,
+            val_loader,
+            test_loader,
+            loss_func,
+            optimizer,
+            scheduler,
+        ) = make(config)
+
+        # and use them to train the model
+        train(model, train_loader, val_loader, loss_func, optimizer, scheduler, config)
+
+        # and test its final performance
+        _, test_accuracy = evaluate(model, test_loader, loss_func)
+        wandb.log(
+            {
+                "test/error": 1 - test_accuracy,
+            }
+        )
+
+        # Save model weights
+        model_artifact = wandb.Artifact(
+            "resnet",
+            type="model",
+            description="Residual Neural Network model trained on CIFAR-10 dataset.",
+            metadata=dict(config),
+        )
+
+        torch.save(model.state_dict(), "model.pth")
+
+        wandb.save("model.pth")
+
+        run.log_artifact(model_artifact)
+
+
+# Execute training pipeline
+if __name__ == "__main__":
+    wandb.login()
+
+    config = {
+        "n": 3,
+        "batch_size": 128,
+        "learning_rate": 0.1,
+        "epochs": 182,
+        "weight_decay": 0.0001,
+        "momentum": 0.9,
+    }
+
+    model_pipeline("resnet", config)
